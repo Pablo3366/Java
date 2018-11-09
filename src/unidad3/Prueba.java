26c016b42bbbb190d5ed15c687af08a729d91779
Pablo <pvarber@gmail.com>
2018-11-09 06:50:33 -0600

    Clase

--- src/unidad3/Prueba.java
+++ src/unidad3/Prueba.java
@@ -4,7 +4,12 @@ public class Prueba {
 
     public static void main(String[] args){
         //Fibonacci.imprimirrec(50);
-        Fibonacci.imprimir();
+        //Fibonacci.imprimir();
+        
+        Factorial.imprimir("La factorial es: ",
+                Factorial.calcular(Factorial.capturar("Ingrese numero: ")));
+        
+        System.out.println(Factorial.factrec(5));
     }
     
 }
