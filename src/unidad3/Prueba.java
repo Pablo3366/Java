bf242f0835b3d9ded7199a604c3cd0015a3d969d
Pablo <pvarber33@gmail.com>
2018-11-09 12:58:20 -0600

    Rango Terminado

--- src/unidad3/Prueba.java
+++ src/unidad3/Prueba.java
@@ -3,18 +3,15 @@ package unidad3;
 public class Prueba {
 
     public static void main(String[] args){
-<<<<<<< HEAD
-        Fibonacci.imprimirrec(50);
-        //Fibonacci.imprimir();
-=======
         //Fibonacci.imprimirrec(50);
         //Fibonacci.imprimir();
         
-        Factorial.imprimir("La factorial es: ",
-                Factorial.calcular(Factorial.capturar("Ingrese numero: ")));
+        //Factorial.imprimir("La factorial es: ",
+        //        Factorial.calcular(Factorial.capturar("Ingrese numero: ")));
         
-        System.out.println(Factorial.factrec(5));
->>>>>>> 26c016b42bbbb190d5ed15c687af08a729d91779
+        Range prueba = new Range();
+        prueba.calcular(40);
+        prueba.imprimir("\nLos resultados son:");
     }
     
 }
