5384bcffaa6686ae81a792515142752fd436be69
Pablo <pvarber@gmail.com>
2018-11-13 04:47:18 -0600

    Encrypt ready

---
+++ src/unidad3/Prueba.java
@@ -0,0 +1,20 @@
+package unidad3;
+
+public class Prueba {
+
+    public static void main(String[] args){
+        //Fibonacci.imprimirrec(50);
+        //Fibonacci.imprimir();
+        
+        //Factorial.imprimir("La factorial es: ",
+        //        Factorial.calcular(Factorial.capturar("Ingrese numero: ")));
+        
+        Range prueba = new Range();
+        prueba.calcular(40, 0);
+        prueba.imprimir("\nLos resultados son:");
+
+//        PagoDelAgua prueba = new PagoDelAgua();
+//        prueba.run();
+    }
+    
+}
