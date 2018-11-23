55a17e42e7d87fe507ba307451f7b011ed5c57fd
Pablo <pvarber33@gmail.com>
2018-11-22 21:18:00 -0600

    Tools

--- src/unidad4/Prueba.java
+++ src/unidad4/Prueba.java
@@ -1,24 +1,33 @@
 package unidad4;
 
+import java.util.Scanner;
+import static unidad4.Multiplicacion.calcular;
+import static unidad4.Multiplicacion.capturar;
+import static unidad4.Multiplicacion.imprimir;
+
 public class Prueba {
     public static void main(String[] args){
-        String[] nombres = {"Eduardo", "Sandra", "Diego", "Claudia"};
-        String[] materias = {"Matematicas", "Ingles", "Programacion"};
-        float[][] calif = new float[3][4];
-        float[] promAlum = new float[4];
-        float[] promMat = new float[3];
+        Scanner in = new Scanner(System.in);
         
-        TablaMatriz.capturar(calif, nombres, materias);
+        int[][] mul1, res;
+        int[] mul2;
+        int filas, columnas;
         
-        //Calcula promedios
-        TablaMatriz.avg(calif, promAlum, true);
-        TablaMatriz.avg(calif, promMat, false);
+        System.out.print("Ingrese numero de filas: ");
+        filas = in.nextInt();
+        System.out.print("Ingrese numero de columnas: ");
+        columnas = in.nextInt();
         
-        //Imprime tabla
-        TablaMatriz.imprimir(calif, promAlum, promMat, nombres, materias);
+        mul1 = new int[filas][columnas];
+        res = new int[filas][columnas];
+        mul2 = new int[filas];
         
-        //Imprime mayor y menor
-        TablaMatriz.imprimir(promAlum, nombres, true);
-        TablaMatriz.imprimir(promMat, materias, false);
+        capturar(mul1, true, 10);
+        capturar(mul2, true, 10);
+        
+        calcular(mul1, mul2, res);
+        
+        imprimir(mul1, res, mul2);
+
     }
 }
