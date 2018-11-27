94d13c76638d789db59b8542d542108fe773b8cb
Pablo <29169516+PabloDV343@users.noreply.github.com>
2018-11-27 15:39:47 -0600

    Encapsulamiento

--- src/unidad4/Prueba.java
+++ src/unidad4/Prueba.java
@@ -1,33 +1,7 @@
 package unidad4;
 
-import java.util.Scanner;
-import static unidad4.Multiplicacion.calcular;
-import static unidad4.Multiplicacion.capturar;
-import static unidad4.Multiplicacion.imprimir;
-
 public class Prueba {
     public static void main(String[] args){
-        Scanner in = new Scanner(System.in);
-        
-        int[][] mul1, res;
-        int[] mul2;
-        int filas, columnas;
-        
-        System.out.print("Ingrese numero de filas: ");
-        filas = in.nextInt();
-        System.out.print("Ingrese numero de columnas: ");
-        columnas = in.nextInt();
-        
-        mul1 = new int[filas][columnas];
-        res = new int[filas][columnas];
-        mul2 = new int[filas];
-        
-        capturar(mul1, true, 10);
-        capturar(mul2, true, 10);
-        
-        calcular(mul1, mul2, res);
-        
-        imprimir(mul1, res, mul2);
-
+        Multiplicacion.run();
     }
 }
---
+++ src/unidad4/Tabletas.java
@@ -0,0 +1,66 @@
+package unidad4;
+
+class Tablet{
+    String marca, modelo;
+    int tamañoPantalla;
+    float precio;
+    
+    void Tablet(String marca, String modelo, int tamaño,
+            float precio){
+        setMarca(marca);
+        setModelo(modelo);
+        setPrecio(precio);
+        setTamañoPantalla(tamaño);
+    }
+    
+    public void setMarca(String var){
+        marca = var;
+    }
+    
+    public void setModelo(String var){
+        modelo = var;
+    }
+    
+    public void setTamañoPantalla(int var){
+        tamañoPantalla = var;
+    }
+    
+    public void setPrecio(float var){
+        precio = var;
+    }
+    
+    public String getMarca(){
+        return marca;
+    }
+    
+    public String getModelo(){
+        return modelo;
+    }
+    
+    public int getTamañoPantalla(){
+        return tamañoPantalla;
+    }
+    
+    public float getPrecio(){
+        return precio;
+    }
+    
+}
+
+public class Tabletas {
+
+    public static void main(String[] args) {
+        Tablet x = new Tablet();
+        
+        x.setMarca("Sony Xperia");
+        x.setModelo("X");
+        x.setTamañoPantalla(11);
+        x.setPrecio(3500);
+        
+        System.out.println(x.getMarca());
+        System.out.println(x.getModelo());
+        System.out.println(x.getTamañoPantalla());
+        System.out.println(x.getPrecio());
+    }
+    
+}
